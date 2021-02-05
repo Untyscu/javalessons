db2d61439980635489fe0811f6d107202718afb4
untyscu <untyscu@gmail.com>
2021-02-05 11:34:48 +0200

    05.02.21

---
+++ phylosophy/chapter4/Terms.java
@@ -0,0 +1,18 @@
+package phylosophy.chapter4;
+
+import java.math.*;
+
+public class Terms {
+    public static void main(String[] args) {
+        rangeOut();
+    }
+    public static void rangeOut(){
+        for(int i = 1; i < 101; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void compare(){
+
+    }
+}
